74bad8e0dbfb2aa1d24c88cde0d8d5e97bc868bb
dxhInter <592310050@qq.com>
2024-04-15 20:21:39 -0400

    still working

--- atlas_manager.py
+++ atlas_manager.py
@@ -3,47 +3,30 @@ import json
 import threading
 import dao.mapping as mapping
 from flask import current_app
+import message_helper
 
 ip = "10.20.0.58"
 PORT = 6668
 
-
-def execute_service_order(service_name, result):
-    if result is not None:
-        print(result[0])
-        result = result[1]
-        if result['Status'] == 'Successful':
-            # input = "({},)".format(result['Service Result'])
-            print(result['Service Result'])
-    #     else:
-    #         input = "(1,)"
-    # else:
-    #     input = "(1,)"
-    print(f"order in {service_name}")
-    data = mapping.load_data()
-    services = data.get('services', [])
-    for i in range(len(services)):
-        if services[i]['name'] == service_name:
-            break
-    service = services[i]
-    print(service)
-    print(service['name'])
-    print(service["thing"])
-    print(service["entity"])
-    print(service["space"])
-    message = json.dumps({
+def json_message(service_name: str, thing_id: str, entity_id: str,space_id: str, inputs=()):
+    message = {
         "Tweet Type": "Service call",
-        "Service Name": '{}'.format(service['name']),
-        "Thing ID": "g6",
-        "Entity ID": '{}'.format(service['entity']),
-        "Space ID": "g6Space",
-        "Service Inputs": None
-    })
+        "Service Name": '{}'.format(service_name),
+        "Thing ID": '{}'.format(thing_id),
+        "Entity ID": '{}'.format(entity_id),
+        "Space ID": '{}'.format(space_id),
+    }
 
-    print(f"message is {message}")
+    if inputs is not None:
+        message["Service Inputs"] = '{}'.format(inputs).replace(', ', ',')
+
+    return json.dumps(message)
+
+def send_service_call(service_name,thing, id,space, ip, inputs=None):
+    global PORT
+    message = json_message(service_name,thing, id, space,inputs)
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as st:
-            global ip, PORT
             st.connect((ip, PORT))
             st.sendall(bytes(message, 'utf-8'))
             data = st.recv(1024)
@@ -60,6 +43,61 @@ def execute_service_order(service_name, result):
     except Exception as e:
         print("Unexpected error:", e)
         return False, {"error": "Unexpected error"}
+
+def execute_service_order(service_name, result):
+    if result is not None:
+        print(result[0])
+        result = result[1]
+        if result['Status'] == 'Successful':
+            input = None
+            print(result['Service Result'])
+        else:
+            input = None
+    else:
+        input = None
+    print(f"order in {service_name}")
+    data = mapping.load_data()
+    services = data.get('services', [])
+    for i in range(len(services)):
+        if services[i]['name'] == service_name:
+            break
+    service = services[i]
+    res = send_service_call(service['name'],service['thing'], service['entity'],service['space'], ip, ())
+    print(f"res1 is {res}")
+    # print(service)
+    # print(service['name'])
+    # print(service["thing"])
+    # print(service["entity"])
+    # print(service["space"])
+    # message = json.dumps({
+    #     "Tweet Type": "Service call",
+    #     "Service Name": '{}'.format(service['name']),
+    #     "Thing ID": "g6",
+    #     "Entity ID": '{}'.format(service['entity']),
+    #     "Space ID": "g6Space",
+    #     "Service Inputs": input
+    # })
+    #
+    # print(f"message is {message}")
+    # try:
+    #     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as st:
+    #         global ip, PORT
+    #         st.connect((ip, PORT))
+    #         st.sendall(bytes(message, 'utf-8'))
+    #         data = st.recv(1024)
+    #     # Validate and decode the JSON response
+    #     if data:
+    #         print("Received data:", data)  # Debug line to see what data was received
+    #         return True, json.loads(data.decode())
+    #     else:
+    #         print("No data received")  # Debug line for no data case
+    #         return False, {"error": "No data received"}
+    # except json.JSONDecodeError as e:
+    #     print("JSON decode error:", e)
+    #     return False, {"error": "JSON decode error"}
+    # except Exception as e:
+    #     print("Unexpected error:", e)
+    #     return False, {"error": "Unexpected error"}
 
 
 # 如果result大于设定的阈值，发送消息给thing
@@ -89,11 +127,11 @@ def order(app):
     # print(service_name1)
     print(service_name1)
     res = execute_service_order(service_name1, None)
-    print(f"res is{res}")
-    service_name2 = app['service2']
-    res2 = execute_service_order(service_name2, res)
-    if res2[1]['Status'] == 'Successful':
-        return True, res2[1]['Service Result']
+    # print(f"res is{res}")
+    # service_name2 = app['service2']
+    # res2 = execute_service_order(service_name2, res)
+    # if res2[1]['Status'] == 'Successful':
+    #     return True, res2[1]['Service Result']
 
 
 threadISRunning = False
